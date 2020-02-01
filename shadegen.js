1ef664df2d53f1dd30dcc075a5ff1cdde729f7c8
Ard-Jan van Etten <arte@linux.fritz.box>
2020-02-01 15:55:33 +0100

    added subfolders content

--- shadegen.js
+++ shadegen.js
@@ -1,6 +1,9 @@
 {
-let mcbwidth = window.screen.width;
-let mcbheight = window.screen.height;
+let screenwidth = window.screen.width;
+let screenheight = window.screen.height;
+
+let mcbheight = window.innerHeight/2;
+let mcbwidth = window.innerWidth/2;
 
 let seed = Math.random();
 document.getElementById('shadegenslider').value = seed*255;
@@ -10,26 +13,26 @@ let dist = 0;
 
 
 const savegpu = new GPU();
-let savecalc = savegpu.createKernel(function(mcbwidth,mcbheight,seed,dist){
-    this.color(this.thread.x/mcbwidth,this.thread.y/mcbheight,seed);
-}).setOutput([mcbwidth,mcbheight]).setGraphical(true);
+let savecalc = savegpu.createKernel(function(screenwidth,screenheight,seed,dist){
+    this.color(this.thread.x/screenwidth,this.thread.y/screenheight,seed);
+}).setOutput([screenwidth,screenheight]).setGraphical(true);
 
 const rendergpu = new GPU();
 let rendercalc = savegpu.createKernel(function(mcbwidth,mcbheight,seed,dist){
-    this.color(this.thread.x/mcbwidth,this.thread.y/mcbheight,seed);
-}).setOutput([Math.round(mcbwidth/2),Math.round(mcbheight/2)]).setGraphical(true);
+    this.color(this.thread.x/mcbwidth,this.thread.y/mcbwidth,seed);
+}).setOutput([mcbwidth,mcbheight]).setGraphical(true);
 
 function rendershade(){
   seed=document.getElementById('shadegenslider').value/255;
-  savecalc(mcbwidth,mcbheight,seed,dist);
-	let canvas = savecalc.canvas;
+  rendercalc(mcbwidth,mcbheight,seed,dist);
+	let canvas = rendercalc.canvas;
 
-  document.getElementById('tdshadegen').appendChild(savecalc.canvas);
+  document.getElementById('tdshadegen').appendChild(rendercalc.canvas);
 }
 
 function save(){
 	seed=document.getElementById('shadegenslider').value/255;
-  savecalc(mcbwidth,mcbheight,seed,dist);
+  savecalc(screenwidth,screenheight,seed,dist);
   png = savecalc.canvas.toDataURL('image/png');
 
   let link = document.createElement('a')
