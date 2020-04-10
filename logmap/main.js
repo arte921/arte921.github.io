56420971707ed645b19af03ab585f26673e51e56
arte921 <ardjanve@gmail.com>
2020-04-10 17:03:19 +0200

    updated logmap using script

--- logmap/main.js
+++ logmap/main.js
@@ -7,27 +7,32 @@ var mcbwidth = window.innerWidth;
 canvas.width = mcbwidth
 canvas.height = mcbheight;
 
-var cx = 3.4;
-var cy = 0.6;
-var rx = 0.75;
+var cx = 3;
+var cy = 0.5;
+var rx = 1.1;
 var ry = rx*mcbheight/mcbwidth;
 
 var xres = 2*rx/mcbwidth;
 var yres = 2*ry/mcbheight;
+var it = 100;
 
 function plot(mx,my){
 	pxx = (mx-(cx-rx))/(2*rx)*mcbwidth;
 	pxy = mcbheight-((my-(cy-ry))/(2*ry)*mcbheight);
 	ctx.fillRect(pxx,pxy,1,1);
 }
-var go = new Date();
+
+var go = window.performance.now();
+
 x=cx-rx;
 y=0.5;
+
 while (x<=cx+rx){
-	for(i=0;i<1000;i++){
+	for(i=0;i<it;i++){
 		y=x*y*(1-y);
 		plot(x,y);
 	}
 	x += xres;
 }
-console.log(new Date()-go);
+
+console.log(window.performance.now()-go);
